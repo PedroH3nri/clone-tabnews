4eea7891ce55b4fdf51d55a37491c3e374770d48
Pedro Henrique <pedrohenri1606@gmail.com>
2025-07-25 11:32:00 -0300

    fix bug connections `/api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,35 +4,45 @@ import database  from "infra/database.js"
 
 export default async function migrations(request, response) {
 
+  if(request.method !== 'GET' && request.method !== 'POST'){
+    return response.status(405).end();
+  }
+
   const dbClient = await database.getNewClient();
+  
+  try{
+    
+    const defaultMigrationsOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations"
+    };
 
-  const defaultMigrationsOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations"
-  };
+    if(request.method === 'GET'){
+      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+      return response.status(201).json(pendingMigrations);
+    }
 
-  if(request.method === 'GET'){
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    if(request.method === 'POST'){
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false
+      });
+
+      return (migratedMigrations.length > 0)
+        ? response.status(201).json(migratedMigrations)
+        : response.status(200).json(migratedMigrations);
+    }
+
+  } catch(error) {
+
+    return response.status(405).end();
+
+  } finally {
+
     await dbClient.end();
-    return response.status(201).json(pendingMigrations);
   }
-
-  if(request.method === 'POST'){
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false
-    });
-
-    await dbClient.end();
-
-    return (migratedMigrations.length > 0)
-      ? response.status(201).json(migratedMigrations)
-      : response.status(200).json(migratedMigrations);
-  }
-
-  return response.status(405).end();
 }
--- tests/api/v1/migrations/post.test.js
+++ tests/api/v1/migrations/post.test.js
@@ -4,6 +4,8 @@ test("POST to /api/v1/migrations should return 200", async () => {
 
   const response2 = await fetch("http:localhost:3000/api/v1/migrations", {method: "POST"});
 
+  const response3 = await fetch("http:localhost:3000/api/v1/migrations", {method: "DELETE"});
+
   const response1Body = await response1.json();
   const response2Body = await response2.json();
 
@@ -13,4 +15,6 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(response2.status).toBe(200);
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
+
+  expect(response3.status).toBe(405);
 });
