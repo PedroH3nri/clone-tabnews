1296563a8ccabeacb4e184550da7f316b011fa8c
Pedro Henrique <pedrohenri1606@gmail.com>
2025-08-05 17:31:55 -0300

    style: alter `status_code` in `post.test.js`

--- tests/api/v1/status/post.test.js
+++ tests/api/v1/status/post.test.js
@@ -6,15 +6,13 @@ beforeAll(async () => {
 
 describe("POST to /api/v1/status", () => {
   describe("Anonymous user", () => {
-    test("should return 200", async () => {
+    test("should return 405", async () => {
       const response = await fetch("http:localhost:3000/api/v1/status", {
         method: "POST",
       });
       expect(response.status).toBe(405);
 
       const responseBody = await response.json();
-
-      console.log(responseBody);
 
       expect(responseBody).toEqual({
         name: "MethodNotAllowedError",
